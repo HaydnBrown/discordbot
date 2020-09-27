37d41553d17388bed7ee1f2cd7cecb0b1bd7d33f
Haydn Brown <haydnpbrown1@gmail.com>
2020-09-26 23:42:48 -0400

    added token now that repo is private

--- discordbot.py
+++ discordbot.py
@@ -37,4 +37,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run('')
+client.run('NzU5MjE1NDgwOTI5NjQ4NzIx.X26QhA.oT5-bMTd_0QIYxeawOOWod0jAiY')
