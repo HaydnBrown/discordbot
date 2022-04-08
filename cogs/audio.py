cdce10b3a523c83d0bd4952f953389ebf7f7fcd4
Haydn Brown <haydnpbrown1@gmail.com>
2022-04-08 19:33:41 -0400

    added ascii cmds in image cog

--- cogs/audio.py
+++ cogs/audio.py
@@ -187,6 +187,19 @@ class Audio(commands.Cog):
                 voice = await voiceChannel.connect()
             voice.play(discord.FFmpegPCMAudio('audio/misc/3pirates.mp3'))
 
+    @commands.command()
+    async def goober(self, ctx):
+        voiceChannel = ctx.message.author.voice.channel
+        if not voiceChannel:
+            await ctx.send("Please connect to a voice channel first")
+        else:
+            voice = discord.utils.get(self.client.voice_clients, guild=ctx.guild)
+            if voice and voice.is_connected():
+                await voice.move_to(voiceChannel)
+            else:
+                voice = await voiceChannel.connect()
+            voice.play(discord.FFmpegPCMAudio('audio/misc/goober-trim.mp3'))
+
 
 def setup(client):
     client.add_cog(Audio(client))
