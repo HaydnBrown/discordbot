74012f1031e98d305dd1d695d257d62fb613836a
Haydn Brown <haydnpbrown1@gmail.com>
2022-04-13 19:19:34 -0400

    added createBet method and added docstrings for commands in betting cog

--- cogs/betting.py
+++ cogs/betting.py
@@ -10,6 +10,14 @@ import datetime
 from urllib.error import HTTPError
 
 
+def generate_code():
+    chars = "1234567890abcdefghijklmnopqrstuvwxyz"
+    list_str = ""
+    for i in range(0, 6):
+        list_str += random.choice(chars)
+    return list_str
+
+
 class Betting(commands.Cog):
 
     def __init__(self, client):
@@ -19,7 +27,80 @@ class Betting(commands.Cog):
         self.mongo_collection = self.mongo_db.ServersCollections
 
     @commands.command()
-    async def setPoints(self, ctx, points):
+    async def initBetting(self, ctx):
+        """
+        Initializes the betting doc in the database. Admin only.
+        """
+        print("\nInitializing betting in the db")
+        if ctx.author.id == 136259896365678593:
+            print("access granted...")
+            betting_doc = await self.mongo_collection.find_one({'_id': 'betting'})
+            if betting_doc is None:
+                await self.mongo_collection.insert_one({'_id': 'betting', 'bets': []})
+            else:
+                print("Betting in the db is already initialized")
+        else:
+            await ctx.send(content="not an admin")
+
+    @commands.command()
+    async def createBet(self, ctx, *args):
+        """
+        Creates a new bet with a unique 6-digit code to access it. Requires two result options to be specified.
+        Usage: Specify the options by typing two phrases separated by ";;;"
+        Format: <Option 1> ;;; <Option 2>
+        Example: Over 2.5 attempts ;;; under 2.5 attempts
+        """
+        bet_id = generate_code()
+        print("Creating a new bet with id: {}".format(bet_id))
+        index_of_seperator = -1  # after the loop, represents the start of second option
+        if ";;;" in args:
+            print("Format for bet was correct")
+            index_of_seperator = args.index(";;;")
+            args.remove(";;;")
+        else:
+            print("format was wrong, checking for typo")
+            for i in range(0, len(args)):
+                if ";;;" in args[i]:
+                    print("found the typo word")
+                    if args[i].endswith(";;;"):
+                        print("this is the end of option 1")
+                        args[i].strip(";")
+                        index_of_seperator = i + 1
+                    else:
+                        # the word starts with ;;;
+                        print("this is the start of option 2")
+                        args[i].strip(";")
+                        index_of_seperator = i
+        if index_of_seperator == -1:
+            print("Could not find ;;; in input")
+            await ctx.send(content="{}, bad input, please try again. Use #help createBet for help on how to create a "
+                                   "bet".format(ctx.author.name))
+            return None
+        else:
+            # we have the index, can create the bet.
+            print("creating the bet...")
+            option_one = ""
+            for i in range(0, index_of_seperator):
+                option_one = option_one + args[i] + " "
+            option_two = ""
+            for i in range(index_of_seperator, len(args)):
+                option_two = option_two + args[i] + " "
+            await self.mongo_collection.update_one({'_id': 'betting'},
+                                                   {'$addToSet': {'bets': {'code': bet_id,
+                                                                           'guild': ctx.guild.id,
+                                                                           'creator': ctx.author.name,
+                                                                           'option_one': option_one,
+                                                                           'option_two': option_two,
+                                                                           'users': []}}})
+            await ctx.send(content="Bet with unique code: {} has been create, {} will be responsible for determining "
+                                   "the outcome and closing the bet".format(bet_id, ctx.author.name)) 
+
+    @commands.command()
+    async def setAllPoints(self, ctx, points):
+        """
+        Sets all user's points to the amount specified. Admin only.
+        :param points: The number of points to set
+        """
         print("\nsetPoints function...")
         if ctx.author.id == 136259896365678593:
             print("access granted...")
@@ -33,6 +114,9 @@ class Betting(commands.Cog):
 
     @commands.command()
     async def initializePoints(self, ctx):
+        """
+        Used to initialize the points of users who are new to a server and haven't had their points set yet. Admin only.
+        """
         print("\nInitializing users points")
         if ctx.author.id == 136259896365678593:
             print("access granted...")
@@ -51,6 +135,10 @@ class Betting(commands.Cog):
 
     @commands.command()
     async def myPoints(self, ctx):
+        """
+        No arguments
+        Return the user's currently available points
+        """
         print("\nmyPoints function called")
         user_list = await self.mongo_collection.find_one({'_id': 'users'})
         member_list = user_list['members']
@@ -61,6 +149,10 @@ class Betting(commands.Cog):
 
     @commands.command()
     async def dailyPoints(self, ctx):
+        """
+        User can claim their daily 250 points every 22 hours. If they try to claim within the 22 hour period they will
+        be notified how much longer the need to wait before claiming again
+        """
         print("\n{}'s daily points".format(ctx.author.name))
         user_list = await self.mongo_collection.find_one({'_id': 'users'})
         member_list = user_list['members']
@@ -82,9 +174,9 @@ class Betting(commands.Cog):
                             time_remaining = datetime.timedelta(
                                 seconds=(79200 - (int(time.time()) - member['lastdaily'])))
                             await ctx.send(content=
-                                           "{} you have {} remaining before you can claim your next daily".format(
-                                               ctx.author.name,
-                                               time_remaining))
+                            "{} you have {} remaining before you can claim your next daily".format(
+                                ctx.author.name,
+                                time_remaining))
                     else:
                         print("User hasnt claimed dailies before, initialize daily time")
                         member['points'] = member['points'] + 250
